ff3c02a3f5db130dfd71245119967b086fb43650
Mátyás Vezér <vezer.m@gmail.com>
2025-10-06 14:59:14 +0200

    nvim: cybuf + alacritty keyboard tweak

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -7,43 +7,59 @@ local plugin_spec = {
       require("catppuccin").setup({
         flavour = "mocha",
         transparent_background = true,
-        term_colors = true
+        term_colors = true,
       })
-      vim.cmd.colorscheme "catppuccin"
-    end
+      vim.cmd.colorscheme("catppuccin")
+    end,
   },
   {
     "supermaven-inc/supermaven-nvim",
-    opts = { keymaps = { accept_suggestion = "<S-Tab>" }, color = { suggestion_color = "#005f5f", cterm = 23 } }
+    opts = { keymaps = { accept_suggestion = "<S-Tab>" }, color = { suggestion_color = "#005f5f", cterm = 23 } },
   },
   {
     "stevearc/oil.nvim",
-    opts = { skip_confirm_for_simple_edits = true, watch_for_changes = true, view_options = { show_hidden = true } }
+    opts = { skip_confirm_for_simple_edits = true, watch_for_changes = true, view_options = { show_hidden = true } },
   },
-  { "numToStr/Navigator.nvim",        opts = {} },
+  { "numToStr/Navigator.nvim", opts = {} },
   {
     "stevearc/conform.nvim",
-    config = require("plugin_setup/conform")
+    config = require("plugin_setup/conform"),
   },
   { "MeanderingProgrammer/render-markdown.nvim", opts = { file_types = { "markdown", "codecompanion" } } },
-  { "mason-org/mason.nvim",                      opts = {} },
+  { "mason-org/mason.nvim", opts = {} },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = {
-      ensure_installed = { "clangd", "bash-language-server", "json-lsp", "lua-language-server", "eslint-lsp", "eslint_d", "prettier", "prettierd", "typescript-language-server", "yaml-language-server" }
-    }
+      ensure_installed = {
+        "clangd",
+        "bash-language-server",
+        "json-lsp",
+        "lua-language-server",
+        "eslint-lsp",
+        "eslint_d",
+        "prettier",
+        "prettierd",
+        "typescript-language-server",
+        "yaml-language-server",
+      },
+    },
   },
   { "kevinhwang91/nvim-bqf", opts = {} },
-  { "zk-org/zk-nvim",        config = function() require("zk").setup({ picker = "fzf_lua" }) end },
-  { "folke/flash.nvim",      opts = { labels = "neioarst" } },
+  {
+    "zk-org/zk-nvim",
+    config = function()
+      require("zk").setup({ picker = "fzf_lua" })
+    end,
+  },
+  { "folke/flash.nvim", opts = { labels = "neioarst" } },
   {
     "ibhagwan/fzf-lua",
-    opts = { max_perf = true, winopts = { height = 1, width = 1 }, keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } }
+    opts = { max_perf = true, winopts = { height = 1, width = 1 }, keymap = { fzf = { ["ctrl-q"] = "select-all+accept" } } },
   },
-  { "j-hui/fidget.nvim",         opts = { progress = { ignore_done_already = true } } },
-  { "Robitx/gp.nvim",            config = require("plugin_setup/gp") },
-  { "nvim-lualine/lualine.nvim", config = require("plugin_setup/lualine"),            dependencies = { "nvim-tree/nvim-web-devicons" } },
-  { "chentoast/marks.nvim",      opts = { mappings = { delete_line = "M" } } },
+  { "j-hui/fidget.nvim", opts = { progress = { ignore_done_already = true } } },
+  { "Robitx/gp.nvim", config = require("plugin_setup/gp") },
+  { "nvim-lualine/lualine.nvim", config = require("plugin_setup/lualine"), dependencies = { "nvim-tree/nvim-web-devicons" } },
+  { "chentoast/marks.nvim", opts = { mappings = { delete_line = "M" } } },
   {
     "folke/noice.nvim",
     event = "VeryLazy",
@@ -51,8 +67,9 @@ local plugin_spec = {
     dependencies = {
       "MunifTanjim/nui.nvim",
       "rcarriga/nvim-notify",
-    }
-  }
+    },
+  },
+  { "ghillb/cybu.nvim", dependencies = { "nvim-tree/nvim-web-devicons", "nvim-lua/plenary.nvim" }, opts = {} },
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -62,7 +79,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
