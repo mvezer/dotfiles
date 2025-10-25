9c3f0ffbbf3f988a3be489b0d00932a88f107f61
Matyas Vezer <vezer.m@gmail.com>
2025-10-25 21:33:09 +0200

    nvim: add lualine

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -123,7 +123,76 @@ core.plugins.add("https://github.com/kylechui/nvim-surround", "nvim-surround")
 core.plugins.add("https://github.com/chrisgrieser/nvim-recorder", "nvim-recorder")
 
 -- Fancy bufferline
-core.plugins.add("https://github.com/akinsho/bufferline.nvim", "bufferline")
+core.plugins.add("https://github.com/akinsho/bufferline.nvim", "bufferline", {
+  options = {
+    style_preset = 4,
+    show_buffer_close_icons = false,
+    color_icons = false,
+    indicator = { style = "underline" },
+    pick = {
+      alphabet = "neioarstgmluyqwfpbj",
+    },
+  },
+})
 
 -- Marks helper
 core.plugins.add("https://github.com/chentoast/marks.nvim", "marks")
+
+-- lualine
+local function filename_plus_project()
+  local dir = vim.fn.getcwd() or ""
+  local buffer_dir = vim.fn.expand("%")
+  local project = "[" .. string.match(dir, ".+/(.+)$") .. "]"
+  local relative_path = buffer_dir:gsub(dir, "")
+  local modified_suffix = ""
+  if vim.bo.modified then
+    modified_suffix = "[+]"
+  end
+  return project .. " " .. relative_path .. " " .. modified_suffix
+end
+
+local function autoformat()
+  if vim.g.disable_autoformat == true or vim.b.disable_autoformat == true then
+    return "-"
+  end
+
+  return "󰦪"
+end
+
+local function supermaven()
+  if require("supermaven-nvim.api").is_running() then
+    return "󰬀"
+  else
+    return "-"
+  end
+end
+core.plugins.add("https://github.com/nvim-lualine/lualine.nvim", "lualine", {
+  options = {
+    always_show_tabline = true,
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = "",
+    section_separators = "",
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
+    lualine_c = { filename_plus_project },
+    lualine_x = { autoformat, supermaven },
+    lualine_y = { { "filetype", icon_only = true }, { "lsp_status", icon = "󰬓" } },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {},
+})
