c7d162df6658b3d6655f07865f881eb5569c9993
Mat Vezer <mat@natix.io>
2025-11-10 19:55:36 +0100

    work: add the telepresence alias

--- zsh/.zsh_includes/work.zsh
+++ zsh/.zsh_includes/work.zsh
@@ -1,3 +1,5 @@
-alias pfw="port-forward.sh stop && port-forward.sh start"
+# alias pfw="port-forward.sh stop && port-forward.sh start"
 
 alias awsdev='export AWS_PROFILE=dev && aws sso login --profile dev && aws sts get-caller-identity --profile dev | jq -r ".Arn" && aws eks update-kubeconfig --region eu-central-1 --name natix-dev-eks --profile dev && echo "Switched to dev "'
+
+alias pfw='telepresence connect --context arn:aws:eks:eu-central-1:739275469289:cluster/natix-dev-eks -n driveand'
