b1da47d13839f5cbf504f876eb1730f9321ecb75
Mat Vezer <mat@natix.io>
2025-11-25 08:25:56 +0100

    zsh: add qr code generation

--- zsh/.zsh_includes/work.zsh
+++ zsh/.zsh_includes/work.zsh
@@ -3,3 +3,5 @@
 alias awsdev='export AWS_PROFILE=dev && aws sso login --profile dev && aws sts get-caller-identity --profile dev | jq -r ".Arn" && aws eks update-kubeconfig --region eu-central-1 --name natix-dev-eks --profile dev && echo "Switched to dev "'
 
 alias pfw='telepresence connect --context arn:aws:eks:eu-central-1:739275469289:cluster/natix-dev-eks -n driveand'
+
+alias qr='qrencode -t UTF8'
