e6a295e123d0a09bf77c4ad68998756ebd3b9483
Mat Vezer <mat@natix.io>
2026-01-09 08:11:08 +0100

    work: update k8s settings

--- zsh/.zsh_includes/work.zsh
+++ zsh/.zsh_includes/work.zsh
@@ -2,6 +2,8 @@
 
 alias awsdev='export AWS_PROFILE=dev && aws sso login --profile dev && aws sts get-caller-identity --profile dev | jq -r ".Arn" && aws eks update-kubeconfig --region eu-central-1 --name natix-dev-eks --profile dev && echo "Switched to dev "'
 
+alias awsprod='export AWS_PROFILE=prod && aws sso login --profile prod && aws sts get-caller-identity --profile prod | jq -r ".Arn" && aws eks update-kubeconfig --region eu-central-1 --name natix-prod-eks --profile prod && echo "Switched to prod"'
+
 alias pfw='telepresence connect --context arn:aws:eks:eu-central-1:739275469289:cluster/natix-dev-eks -n driveand'
 
 alias qr='qrencode -t UTF8'
