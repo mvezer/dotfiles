92216a70c9d421150ae536cf28e818b2a2e1c52d
Matyas Vezer <vezer.m@gmail.com>
2026-03-11 10:54:34 +0100

    zsh: home assistant functions

--- zsh/.zsh_includes/linux.zsh
+++ zsh/.zsh_includes/linux.zsh
@@ -4,3 +4,20 @@ alias battery="cat /sys/class/power_supply/BAT0/capacity"
 alias cb="xclip -selection c"
 alias mixer="wiremix -v output"
 alias proxmox="ssh root@192.168.178.173"
+alias jellyfin="ssh root@192.168.178.175"
+
+function evening() {
+  hctl on szekreny
+  hctl on golyo_lampak
+}
+
+function dinner() {
+  evening
+  hctl brightness asztal 30
+}
+
+function night() {
+  hctl off szekreny
+  hctl off golyo_lampak
+  hctl off asztal
+}
