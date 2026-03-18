02da4069b11ebf1e6986d2b7d565b747c95e365f
Matyas Vezer <mat@vezer.me>
2026-03-18 15:54:01 +0100

    nvim: fix render markdown codeblocks highlight colors

--- nvim/.config/nvim-12/after/plugin/render-markdown.lua
+++ nvim/.config/nvim-12/after/plugin/render-markdown.lua
@@ -1,3 +1,6 @@
 require("render-markdown").setup({
 	file_types = { "markdown", "codecompanion" },
 })
+
+-- Fix the codeblock background color
+vim.cmd("hi RenderMarkdownCode guifg=white guibg=black")
