ce40d668f602c9cf263e70defeccf2475a6e05e0
Matyas Vezer <vezer.m@gmail.com>
2026-04-07 15:29:25 +0200

    nvim: change neovide font size to 10

--- nvim/.config/nvim/lua/neovide.lua
+++ nvim/.config/nvim/lua/neovide.lua
@@ -1,4 +1,4 @@
-vim.o.guifont = "JetBrainsMono Nerd Font Mono:h11" -- text below applies for VimScript
+vim.o.guifont = "JetBrainsMono Nerd Font Mono:h10" -- text below applies for VimScript
 vim.g.neovide_scale_factor = 1.0
 
 vim.api.nvim_create_user_command("NeovideIncreaseScaleFactor", function()
