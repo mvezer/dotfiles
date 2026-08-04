6477253b34bcc6a22c296149d998372f12f66a82
Matyas Vezer <mat@MacBook-Pro.fritz.box>
2026-08-04 08:22:29 +0200

    tmux+nvim: the block cursor fix

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -131,3 +131,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
 		vim.fn.chdir(root)
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
+	callback = function()
+		vim.opt.guicursor = ""
+		vim.fn.chansend(vim.v.stderr, "\x1b[ q")
+	end,
+	desc = "Restore terminal cursor",
+})
