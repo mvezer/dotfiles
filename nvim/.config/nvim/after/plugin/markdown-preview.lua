e31add05bdb1e4b6f7c0675cac2e27479d34c9b4
Matyas Vezer <matyas.vezer@bikeleasing.de>
2026-08-23 22:54:06 +0200

    nvim: minimal ff based markdown preview

--- nvim/.config/nvim/after/plugin/markdown-preview.lua
+++ nvim/.config/nvim/after/plugin/markdown-preview.lua
@@ -1,6 +1,6 @@
 vim.cmd([[
 function OpenMarkdownPreview (url)
-  call system('open -n -a "Brave Browser" --args --new-window ' . shellescape(a:url))
+  call system('open -n -a "Firefox" --args --no-remote -P mdpreview --new-window ' . shellescape(a:url))
 endfunction
 ]])
 vim.g.mkdp_browserfunc = "OpenMarkdownPreview"
