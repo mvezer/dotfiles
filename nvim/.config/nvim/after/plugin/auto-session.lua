1a1ab9a2b36a807f53b4c1d171ce43b94e78bbc9
Matyas Vezer <vezer.m@gmail.com>
2026-08-30 00:05:43 +0200

    nvim: remove the .dotfiles from the auto-session scope

--- nvim/.config/nvim/after/plugin/auto-session.lua
+++ nvim/.config/nvim/after/plugin/auto-session.lua
@@ -1,7 +1,8 @@
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 require("auto-session").setup({
-	allowed_dirs = { "~/workspace", "~/.dotfiles", "~/workshop" },
+	allowed_dirs = { "~/workspace", "~/workshop" },
 	session_lens = {
 		picker = "fzf",
 	},
+	git_use_branch_name = true,
 })
